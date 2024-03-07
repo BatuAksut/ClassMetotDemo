e73be831cfed7c3ba325bf2c051dfd3155042126
Batuhan Aksüt <batuhanaksut@hotmail.com>
2024-03-07 18:45:28 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+       public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " adlı müşteri eklendi");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using ClassMetotDemo;
+
+
+
+Musteri musteri = new Musteri();
+
+musteri.Ad = "Ahmet";
+musteri.Soyad = "Türkoğlu";
+musteri.Id = 1;
+
+MusteriManager musteriManager = new MusteriManager();
+
+musteriManager.Ekle(musteri);
